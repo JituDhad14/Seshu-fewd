f81eca8e57f74a0a7321c68225488bfe4befdb50
jitendra <jitudhaduvai@gmail.com>
2025-06-18 10:24:04 +0530

    first

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import StudentMarks from './StudentMarks';
+
+function App() {
+  return (
+    <div className="App">
+      <StudentMarks />
+    </div>
+  );
+}
+
+export default App;
